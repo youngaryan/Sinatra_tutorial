7a460c807733cf68ef13c740ebddf805047cd384
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-03-04 13:26:28 +0000

    first day of sinatra tut

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+IMAGES = [
+  {title: "Programmer", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAZpdMyuDahkrU01rONtOS8p1ItTdJb_5Ncg&usqp=CAU"},
+  {title: "Uni", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9usp9i-pEgD9G7C-rPr14KyOvaFt0-FX40Q&usqp=CAU"}
+]
+
+get '/images' do
+  @images = IMAGES
+  erb :images
+end
+
+get "/images/:index" do |index|
+  @image = IMAGES[index.to_i]
+  erb :image
+end
+
